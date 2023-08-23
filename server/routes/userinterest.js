13a4a9b4c9d41ca1674c5ad9153ace5f19833bc5
Janani-valluru <100375042+Janani-valluru@users.noreply.github.com>
2023-08-23 18:38:02 +0530

    first

--- server/routes/userinterest.js
+++ server/routes/userinterest.js
@@ -6,7 +6,12 @@ const UserInterest = require('../models/userinterest');
 // Create a new user interest
 router.post('/', async (req, res) => {
   try {
-    const { firstname,lastname,email,mobile, age, education, interests, location,walkingSpeed,runningSpeed,swimmingLocation,moviegenre } = req.body;
+    const { firstname,lastname,email,mobile, age, education, interests, location,walkingSpeed,runningSpeed,swimmingLocation,moviegenre,childcare,
+        eldercare,
+        dogwalking,
+      errands, } = req.body;
+    
+    
     const newUserInterest = new UserInterest({
      firstname,
         lastname,
@@ -19,7 +24,11 @@ router.post('/', async (req, res) => {
         walkingSpeed,
         runningSpeed,
         swimmingLocation,
-        moviegenre,
+      moviegenre,
+        childcare,
+        eldercare,
+        dogwalking,
+        errands,
     });
     await newUserInterest.save();
     res.json(newUserInterest);
