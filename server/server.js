7fdaa1397f8cecce3aa015bddac90cafc42cdab5
Janani V A <janani.valluru@gmail.com>
2023-09-04 17:54:38 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ const userDetailsRoutes = require('./routes/userdetails');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://interest-frontend-dp0i9w7gx-janani-valluru.vercel.app/"],
+    methods:["POST","GET"],
+    credentials:true
+));
 app.use(bodyParser.json());
 
 // Connect to MongoDB Atlas
