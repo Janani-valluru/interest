c655d5baef97fb440100dab16fe96fc7b0cac60c
Janani V A <janani.valluru@gmail.com>
2023-09-04 18:01:16 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const app = express();
 // Middleware
 app.use(cors(
   {
-    origin: ["https://interest-frontend-dp0i9w7gx-janani-valluru.vercel.app/"],
+    origin: ["https://interest-frontend-dp0i9w7gx-janani-valluru.vercel.app"],
     methods:["POST","GET"],
     credentials:true
 ));
